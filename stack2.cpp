11f58b7f17adab9848a8eef5044d1a119a479fb4
Bhuvan7888 <bhuvanmalhotra11@gmail.com>
2025-09-14 16:47:22 +0530

    Update stack2.cpp

--- stack2.cpp
+++ stack2.cpp
@@ -1,45 +1,26 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<stack>
 using namespace std;
 
-#define SIZE 100   
 
-char st[SIZE];
-int top = -1;
-
-
-void push(char ch) {
-    if (top == SIZE - 1) {
-        cout << "Stack Overflow!\n";
-        return;
-    }
-    st[++top] = ch;
-}
-
-
-char pop() {
-    if (top == -1) {
-        cout << "Stack Underflow!\n";
-        return '\0';
-    }
-    return st[top--];
-}
 
 int main() {
-    string str, reversed = "";
-    cout << "Enter a string: ";
-    cin >> str;
+    string str = "Bhuvan Malhotra";
+    stack<char> st;
 
-   
-    for (char c : str) {
-        push(c);
+    for(char c : str){
+        st.push(c);
     }
 
-    
-    while (top != -1) {
-        reversed += pop();
+    string reversed = "" ;
+
+    while(!st.empty()){
+        reversed+=st.top();
+        st.pop();
     }
 
-    cout << "Reversed string: " << reversed << endl;
+    cout << "The Original string is : " << str << endl;
+    cout << "The reversed string is :" << reversed << endl;
+
     return 0;
 }
